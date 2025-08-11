70f19bbd423e5170ad994fc60f8a782201ac9ea7
DevDaniel21 <danielglaucia2.00@gmail.com>
2025-08-11 17:42:30 -0300

    Layout feito

--- App.js
+++ App.js
@@ -1,61 +1,128 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, useColorScheme } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function App() {
-  const colorScheme = useColorScheme();
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Text style={styles.logo}>Logo</Text>
+                <View style={styles.menu}></View>
+            </View>
 
-  const themeTextStyle = colorScheme === 'light' ? styles.lightThemeText : styles.darkThemeText;
-  const themeContainerStyle = colorScheme === 'light' ? styles.lightContainer : styles.darkContainer;
-  let tema = colorScheme === 'light' ? 'claro' : 'escuro';
+            <View style={styles.content}>
+                <View style={styles.card_container}>
+                    <View style={styles.card_image}></View>
+                    <View style={styles.card_info}>
+                        <Text style={styles.card_tittle}>Titulo card</Text>
+                        <Text style={styles.card_description}>
+                            Descricao bem legal
+                        </Text>
+                    </View>
+                </View>
+                <View style={styles.card_container}>
+                    <View style={styles.card_image}></View>
+                    <View style={styles.card_info}>
+                        <Text style={styles.card_tittle}>Titulo card</Text>
+                        <Text style={styles.card_description}>
+                            Descricao bem legal
+                        </Text>
+                    </View>
+                </View>
+                <View style={styles.button_container}>
+                    <Text style={styles.button_text}>Ver mais</Text>
+                </View>
+            </View>
 
-  return (
-    <View style={[styles.container, themeContainerStyle]}>
-      <Text style={[styles.p, themeTextStyle]}>Você está usando o tema {tema}</Text>
-      <Text style={styles.tituloPrincipal}>Título Principal</Text>
-      <Text style={styles.subTitulo}>Subtítulo!</Text>
-      <View style={styles.box1}></View>
-      <Text style={styles.saibaMais}>Saiba Mais</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+            <View style={styles.footer}>
+                <Text style={styles.footer_text}>Daniel Custódio</Text>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  lightContainer: {
-    backgroundColor: '#fff',
-  },
-  darkContainer: {
-    backgroundColor: '#000',
-  },
-  lightThemeText: {
-    color: '#000',
-  },
-  darkThemeText: {
-    color: '#fff',
-  },
-  tituloPrincipal: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginVertical: 20,
-    color: 'green'
-  },
-  subTitulo: {
-    fontSize: '18px',
-    fontWeight: 'bold',
-    color: 'red'
-  },
-  saibaMais: {
-    fontSize: '14px',
-    color: 'grey',
-  },
-  box1: {
-    width: '100%',
-    height: 100,
-    backgroundColor: 'slateblue',
-  }
+    container: {
+        flex: 2,
+        backgroundColor: '#295dacff',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    header: {
+        flex: 1,
+        width: '100%',
+        backgroundColor: '#2344b1ff',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flexDirection: 'row',
+        paddingHorizontal: 20,
+    },
+    logo: {
+        color: 'white',
+        fontSize: 20,
+    },
+    content: {
+        flex: 7,
+        width: '100%',
+        backgroundColor: '#0f3274ff',
+        paddingVertical: '3%',
+        paddingHorizontal: '6%',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+    },
+    footer: {
+        flex: 1,
+        width: '100%',
+        backgroundColor: '#2344b1ff',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    menu: {
+        width: 40,
+        height: 40,
+        backgroundColor: '#201f1fff',
+    },
+    card_container: {
+        height: 100,
+        width: '100%',
+        backgroundColor: '#297cacff',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flexDirection: 'row',
+        paddingHorizontal: '3%',
+        paddingVertical: '1%',
+        marginTop: 10,
+        borderRadius: 10,
+    },
+    card_image: {
+        width: 80,
+        height: 80,
+        backgroundColor: '#000000',
+        borderRadius: 6,
+    },
+    card_info: {
+        flex: 1,
+        height: '100%',
+        paddingHorizontal: '3%',
+        paddingVertical: '3%',
+    },
+    card_tittle: {
+        fontSize: 21,
+        fontWeight: 'bold',
+        color: 'white',
+    },
+    card_description: {
+        fontSize: 15,
+        color: 'whitesmoke',
+    },
+    button_container: {
+        width: 100,
+        height: 30,
+        backgroundColor: '#ffffffff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    button_text: {},
+    footer_text: {
+        fontSize: 20,
+        color: 'white',
+    },
 });
